505d105b7d137f68a4c28e07e9e89ac538e71ec1
Michael Whittaker <mjw297@cornell.edu>
2015-10-26 21:35:05 -0700

    Added utility to print json NetKAT policies.
    Note that the printer **will not** work when using the usual frenetic
    libraries. You need to be using my frenetic fork which has the
    appropriate fixes to the json parsing stuff [1].
    
    [1]: https://github.com/mwhittaker/frenetic

---
+++ src/PrintPolicy.ml
@@ -0,0 +1,15 @@
+open Core.Std
+open Async.Std
+
+let main (policy_file: string) : unit Deferred.t =
+  Reader.file_contents policy_file >>| fun policy_string ->
+  Frenetic_NetKAT_Json.policy_from_json_string policy_string
+  |> Frenetic_NetKAT_Pretty.string_of_policy
+  |> print_endline
+
+let () =
+  Command.(async
+    ~summary:"Compile path queries to NetKAT terms"
+    Spec.(empty +> anon ("policy_json_file" %: file))
+    (fun policy_file () -> main policy_file)
+  ) |> Command.run
