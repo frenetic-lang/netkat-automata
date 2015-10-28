a3d35580eb000541fd29c7fa8d7e2dbf04a271bc
Rene Zhang <rz99@cornell.edu>
2015-10-27 22:52:29 -0700

    changed echoing to occur after task finishes

--- bench.sh
+++ bench.sh
@@ -1,1 +1,1 @@
-parallel -a topologies.txt --verbose './Compile.native zoo topozoo/{}.json topozoo/{}.dot queries/* > bench/{}.txt'
+parallel -a topologies.txt './Compile.native zoo topozoo/{}.json topozoo/{}.dot queries/* > bench/{}.txt; echo "{} done"'
