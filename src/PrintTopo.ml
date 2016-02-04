ee537b0bcd124cb01f37f440dd5f7b0917fe68cf
Michael Whittaker <mjw297@cornell.edu>
2016-02-04 12:30:28 -0800

    PrintTopo prints number of vertices and switches.
    We needed to know the largest number of switches in any topology, so I
    changed the PrintTopo script to print out the number of switches.

--- src/PrintTopo.ml
+++ src/PrintTopo.ml
@@ -7,6 +7,13 @@ module Net = Frenetic_Network.Net
 
 let main (topo_file: string) : unit Deferred.t =
   let topo = Net.Parse.from_dotfile topo_file in
+  let num_vertexes = Net.Topology.num_vertexes topo in
+  let num_switches =
+    Net.Topology.vertexes topo
+    |> Net.Topology.VertexSet.count ~f:(fun n ->
+        let label = Net.Topology.vertex_to_label topo n in
+        Frenetic_Network.Node.(device label = Switch))
+  in
   print_endline (Net.Pretty.to_string topo);
   print_endline "";
   print_endline (Ast.Term.to_string (Measurement.term_of_topology topo));
@@ -14,6 +21,9 @@ let main (topo_file: string) : unit Deferred.t =
   print_endline (Ast.Term.to_string (Measurement.in_of_topology topo));
   print_endline "";
   print_endline (Ast.Term.to_string (Measurement.out_of_topology topo));
+  print_endline "";
+  printf "%d vertices\n" num_vertexes;
+  printf "%d switches\n" num_switches;
   return ()
 
 let () =
