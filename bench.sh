90a0d2e82191c2d1a9a1d53ddbece878a7ba2a55
Michael Whittaker <mjw297@cornell.edu>
2016-02-19 12:22:24 -0800

    Bench uses parallel + topos.

--- bench.sh
+++ bench.sh
@@ -2,24 +2,27 @@
 
 set -euo pipefail
 
+f() {
+    topo="$(basename $1 .json)"
+    date
+    echo $topo start
+    ./Compile.native zoo topozoo/"$topo".{json,dot} \
+        queries/no_paths.txt \
+        queries/1edge.txt \
+        queries/5edge.txt \
+        queries/1allstar.txt \
+        queries/5allstar.txt \
+        queries/node4or5.txt \
+        queries/path_123_456_789_101112.txt \
+        queries/port80.txt \
+        > "bench/$topo.txt"
+    date
+    echo "$topo done"
+}
+
 main() {
-    for topofile in "$@"; do
-        topo="$(basename $topofile .json)"
-        date
-        echo $topo start
-        ./Compile.native zoo topozoo/"$topo".{json,dot} \
-            queries/no_paths.txt \
-            queries/1edge.txt \
-            queries/5edge.txt \
-            queries/1allstar.txt \
-            queries/5allstar.txt \
-            queries/node4or5.txt \
-            queries/path_123_456_789_101112.txt \
-            queries/port80.txt \
-            > "bench/$topo.txt"
-        date
-        echo "$topo done"
-    done
+    export -f f
+    parallel f ::: "$@"
 }
 
 main "$@"
